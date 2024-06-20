a2f97d8b1552ad320eb941738e5290f7f620599b
junjungoal <junyamada107@gmail.com>
2024-06-20 15:27:00 +0100

    update docker

--- docker/build.sh
+++ docker/build.sh
@@ -1,1 +1,1 @@
-docker build -t ramp-sim:latest --build-arg USER=${USER} --build-arg PASSWD=root --build-arg USER_ID=$(id -u) .
+docker build -t ramp-sim-curobo:latest --build-arg USER=${USER} --build-arg PASSWD=root --build-arg USER_ID=$(id -u) .
--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,5 @@
 xhost +
-docker run --name ramp-sim --entrypoint bash -it -d --gpus all -e "ACCEPT_EULA=Y" -e HOST_UID=${UID} -e USER=${USER} -e DISPLAY=$DISPLAY --network=host --privileged \
+docker run --name ramp-sim-curobo --entrypoint bash -it -d --gpus all -e "ACCEPT_EULA=Y" -e HOST_UID=${UID} -e USER=${USER} -e DISPLAY=$DISPLAY --network=host --privileged \
     -e "PRIVACY_CONSENT=Y" \
     -v /home/$USER/.Xauthority:/home/$USER/.Xauthority \
     -v /home/$USER/projects/:/home/$USER/projects/ -v /home/$USER/.ssh/:/home/$USER/.ssh/ --shm-size=1gb \
@@ -13,4 +13,4 @@ docker run --name ramp-sim --entrypoint bash -it -d --gpus all -e "ACCEPT_EULA=Y
     -v ~/docker/isaac-sim/documents:/home/$USER/Documents:rw \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /dev:/dev \
-    ramp-sim
+    ramp-sim-curobo
